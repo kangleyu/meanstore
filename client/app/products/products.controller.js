f79b324f112099e6f5b556505f24bd4ee98ae960
Tom Yu <kangleyu@gmail.com>
2016-04-26 22:13:10 +0800

    add frontend UI

--- client/app/products/products.controller.js
+++ client/app/products/products.controller.js
@@ -2,5 +2,26 @@
 
 angular.module('meanstoreApp')
   .controller('ProductsCtrl', function ($scope, products) {
-    $scope.products = products;
+    $scope.products = products.query();
+  })
+  .controller('ProductViewCtrl', function($scope, $state, $stateParams, products){
+    $scope.product = products.get({ id: $stateParams.id });
+    $scope.deleteProduct = function () {
+      products.delete($scope.product);
+      $state.go('products');
+    }
+  })
+  .controller('ProductNewCtrl', function($scope, $state, products){
+    $scope.product = {};
+    $scope.addProduct = function (product) {
+      products.create($scope.product);
+      $state.go('products');
+    }
+  })
+  .controller('ProductEditCtrl', function($scope, $state, $stateParams, products){
+    $scope.product = products.get({ id: $stateParams.id });
+    $scope.editProduct = function (product) {
+      products.update($scope.product);
+      $state.go('products');
+    }
   });
--- client/app/products/products.service.js
+++ client/app/products/products.service.js
@@ -2,11 +2,59 @@
 
 angular.module('meanstoreApp')
   .factory('products', function () {
-    return [
+    var last_id = 5;
+    var example_products = [
       { _id: 1, title: 'Product 1', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor site ament'},
       { _id: 2, title: 'Product 2', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor site ament'},
       { _id: 3, title: 'Product 3', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor site ament'},
       { _id: 4, title: 'Product 4', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor site ament'},
       { _id: 5, title: 'Product 5', price: 123.45, quantity: 10, description: 'Lorem ipsum dolor site ament'},
     ];
+    
+    return {
+      // get all products
+      query: function() {
+        return example_products;
+      },
+      
+      // get specific product
+      get: function(product) {
+        var result = {};
+        angular.forEach(example_products, function(product){
+          if (product._id == params.id) {
+            return this.product = product;
+          }
+        }, result);
+        return result.product;
+      },
+      
+      // delete product
+      delete: function(params) {
+        angular.forEach(example_products, function(product, index) {
+          if (product._id == params.id) {
+            console.log(product, index);
+            example_products.splice(index, 1);
+            return;
+          }
+        });
+      },
+      
+      // create new product
+      create: function(product) {
+        product.id = ++last_id;
+        example_products.push(product);
+      },
+      
+      // update product
+      update: function(product) {
+        var item = this.get(product);
+        if(!item) return false;
+        
+        item.title = product.title;
+        item.price = product.price;
+        item.quantity = product.quantity;
+        item.description = product.description;
+        return true;
+      }
+    };
   });
